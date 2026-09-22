d66586691fb4dfa33cd27d86bb08982e856a7c51
Ahmadr127 <ahmdrifai808@gmail.com>
2026-09-22 10:28:15 +0700

    .

--- database/seeders/AdminAccount.php
+++ database/seeders/AdminAccount.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use <Img:srcset></Img:srcset>lluminate\Models\User;
+use Illuminate\Models\User;
 
 class AdminAccount extends Seeder
 {
